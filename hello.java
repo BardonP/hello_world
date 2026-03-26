1c7d153fa9ceda6a50ff485d439335fb4f3dc2ee
Brandon <brandonpoulin21@gmail.com>
2026-03-26 11:52:00 -0400

    first commit (fix 3)

--- hello.java
+++ hello.java
@@ -7,7 +7,7 @@ public class hello {
        // initialize variables
        String firstName = "Brandon";
 
-       System.out.println("hello, i am " + firstName);
+       System.out.println("Hello, I am " + firstName);
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
 
